16eccb4f86c9e028c06708c6bb8c63041debacb4
Kaan Atakan Yilmaz <yilmazkaanatakan@gmail.com>
2025-02-01 23:10:14 +0300

    skills components has been added.

---
+++ src/pages/skills/education/index.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { GraduationCap } from "lucide-react";
+import { motion } from "framer-motion";
+function Education() {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="p-5 flex flex-col w-full mb-14 "
+    >
+      <div className="flex flex-col items-center lg:items-start gap-3 mt-4  ">
+        <h1 className="font-bold text-[30px] text-[#15202b] md:text-[40px] text-center md:text-left">
+          Education
+        </h1>
+        <span className="w-32 h-2 bg-[#ff7a00] rounded-full "></span>
+        <span className="w-24 h-2 ml-0  bg-[#ff7a00] rounded-full text-center"></span>
+      </div>
+      <div className="flex justify-center">
+        <div className=" leading-8  font-medium p-3 mt-6 rounded-2xl bg-[#f9f9f9] shadow-lg text-left w-[300px] sm:w-[500px]  md:w-[800px] lg:w-[1000px] xl:w-[1300px] 2xl:w-[1800px]">
+          <div className="flex flex-row  items-center ">
+            <GraduationCap size={35} className="ml-2 text-[#ff7a00]" />
+            <h1 className="text-lg font-bold ml-3 ">2019-2024</h1>
+          </div>
+          <h1 className="text-lg font-bold p-2">
+            Düzce University – Bachelor's Degree in Computer Engineering
+          </h1>
+          <p className="text-lg font-medium p-3">
+            I pursued my Bachelor's degree in Computer Engineering at Düzce
+            University, where I gained a strong foundation in both theoretical
+            and practical aspects of the field. Throughout my studies, I
+            explored key areas such as algorithms, data structures, web
+            development, software engineering, and artificial intelligence. The
+            curriculum provided hands-on experience through various projects,
+            allowing me to apply my knowledge to real-world scenarios.
+            Additionally, I developed problem-solving skills and a deep
+            understanding of computational logic, which have been instrumental
+            in shaping my approach to software development.
+          </p>
+        </div>
+      </div>
+    </motion.div>
+  );
+}
+
+export default Education;
--- src/pages/skills/index.jsx
+++ src/pages/skills/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import Experience from "./experience";
+import Education from "./education";
+import MySkills from "./my-skills";
 function Skills() {
-  return <div>Yetenekler</div>;
+  return (
+    <div className=" bg-[#f2f1fb]  px-20  flex-col justify-center items-center    border-b-3 border-[#15202b]">
+      <Education />
+      <Experience />
+      <MySkills />
+    </div>
+  );
 }
 
 export default Skills;
